9fa112332f05c19e592eae435b4c0b54970804cd
Junyeong Kim <junyeongkim6780@gmail.com>
2025-11-07 02:13:21 +0900

    implemented character movement and interaction with lv1 map

--- Assets/Scripts/Player/PlayerScript2.cs
+++ Assets/Scripts/Player/PlayerScript2.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerScript2 : MonoBehaviour
 {
     private CharacterController mController;
     private Animator mAnimator;
     public Vector3 mVelocity;
-    public float WalkSpeed   = 2f;
-    public float RunSpeed    = 4f;
+    public float WalkSpeed = 2f;
+    public float RunSpeed = 4f;
     public float SprintSpeed = 6f;
-
     private Transform mCameraPivot;
     private bool mWasGrounded = false;
+
+    private Transform mCharacterModel;
+
+    public Vector3 respawnPosition = new Vector3(0.34f, 1.35f, -63f); // jy - respawn location. 
+    public float fallThreshold = -20f; //jy - respawn fall threshold 
 
     // Input Actions
     private InputAction mMoveAction, mWalkAction, mSprintAction, mJumpAction;
 
-    // Start is called before the first frame update
     void Start()
     {
         // Get Components
@@ -32,8 +35,9 @@ public class PlayerScript : MonoBehaviour
         // Set Velocity to 0
         mVelocity = Vector3.zero;
 
-        // Get Camera Pivotc
+        // Get Camera Pivot
         mCameraPivot = transform.Find("CameraPivot");
+        mCharacterModel = transform.Find("Rig"); // have to rotate rotate "rig" not the mesh to work. 
 
         // Get Input Actions
         mMoveAction = InputSystem.actions.FindAction("Move");
@@ -42,13 +46,22 @@ public class PlayerScript : MonoBehaviour
         mJumpAction = InputSystem.actions.FindAction("Jump");
     }
 
-    private void Jump() {
+    private void Jump()
+    {
         // Set Velocity Up
-        mVelocity.y = 8.0f;
+        mVelocity.y = 7.0f;
         mAnimator.SetTrigger("Jump");
     }
+    private void Respawn()
+    {
+        mController.enabled = false; 
+        transform.position = respawnPosition;
+        mController.enabled = true;
+        mVelocity = Vector3.zero;
+    }
 
-    // Update is called once per frame
+    private float mTargetRotationY = 0f;
+
     void Update()
     {
         // Get Input
@@ -56,31 +69,42 @@ public class PlayerScript : MonoBehaviour
 
         // Player Speed
         float targetSpeed = 0.0f;
-        if (move.y > 0.0f)
+        float moveDirection = 0.0f;
+
+        if (move.x != 0.0f)
         {
+            moveDirection = Mathf.Sign(move.x);
+
+            if (moveDirection > 0)
+            {
+                mTargetRotationY = 0f;
+            }
+            else
+            {
+                mTargetRotationY = 180f;
+            }
+
             if (mWalkAction.IsPressed())
             {
-                // Player Walking
                 targetSpeed = WalkSpeed;
             }
             else if (mSprintAction.IsPressed())
             {
-                // Player Sprinting
                 targetSpeed = SprintSpeed;
             }
             else
             {
-                // Player Running
                 targetSpeed = RunSpeed;
             }
         }
+
         // Update Velocity
-        mVelocity.z = Mathf.MoveTowards(mVelocity.z, targetSpeed, 2.0f * Time.deltaTime);
+        mVelocity.z = Mathf.MoveTowards(mVelocity.z, targetSpeed * moveDirection, 20.0f * Time.deltaTime);
 
         // Apply Gravity
         if (mController.isGrounded == false)
         {
-            mVelocity.y -= 9.8f * Time.deltaTime;
+            mVelocity.y -= 7.8f * Time.deltaTime;
         }
         else
         {
@@ -94,20 +118,29 @@ public class PlayerScript : MonoBehaviour
         }
 
         // Update Animator Variables
-        mAnimator.SetFloat("Speed", mVelocity.z);
+        mAnimator.SetFloat("Speed", Mathf.Abs(mVelocity.z));
         mAnimator.SetBool("isGrounded", mController.isGrounded);
 
-        // Jumping
-        if (mJumpAction.WasPerformedThisFrame())
+        if (mJumpAction.WasPerformedThisFrame() && mController.isGrounded)
         {
             Jump();
         }
 
+        
+        if (transform.position.y < fallThreshold)
+        {
+            Respawn();
+        }
+
         // Update whether Character was grounded last frame
-        // (must be before Controller Move)
         mWasGrounded = mController.isGrounded;
 
         // Move Character
         mController.Move(mVelocity * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        mCharacterModel.localRotation = Quaternion.Euler(0, mTargetRotationY, 0);
     }
 }
